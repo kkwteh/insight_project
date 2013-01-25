3cdb8002dea850818c94703d87f9602706d2b09e
Kevin Teh <kkwteh@gmail.com>
2013-01-25 14:07:41 -0800

    refactored twitter_stream script to avoid 'import *'

--- twitter_stream.py
+++ twitter_stream.py
@@ -1,12 +1,11 @@
-from twitter import *
+import twitter
 
 CONSUMER_KEY = 'sLGccwOdfySptswo1ZKErg'
 CONSUMER_SECRET = 'z5V9g6sOJ9BEYhvsSvnzt6pjS7gVWV2komWyIz5XZE'
 oauth_token = "101769689-tPwXbgj96kaYpnCKHSijZJ5r6arePyLlMIQUj4Ts"
 oauth_secret = "ijCLoaw3bfRiOzbR572jKGQe3pYHndIps3CIp9KOWa4"
-auth = OAuth(oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET)
 
-twitter_stream = TwitterStream(auth=OAuth(oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
+twitter_stream = twitter.TwitterStream(auth=twitter.oauth.OAuth(oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
 iterator = twitter_stream.statuses.sample()
 
 for tweet in iterator:
