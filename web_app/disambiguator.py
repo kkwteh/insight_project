403f457443e7b1fded5c6262ee39e657ac07b052
Kevin Teh <kkwteh@gmail.com>
2013-01-26 13:40:28 -0800

    web_app displays vanilla twitter search

---
+++ web_app/disambiguator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form, TextField, HiddenField, ValidationError,\
+                          Required, RecaptchaField
+from flask import request
+
+app = Flask(__name__)
+Bootstrap(app)
+
+app.config['BOOTSTRAP_USE_MINIFIED'] = True
+app.config['BOOTSTRAP_USE_CDN'] = True
+app.config['BOOTSTRAP_FONTAWESOME'] = True
+app.config['SECRET_KEY'] = 'devkey'
+
+class QueryForm(Form):
+    q = TextField(' ',validators=[Required()])
+
+@app.route('/')
+def index():
+    form = QueryForm()
+    query = request.args.get('q')
+    return render_template('index.html', form=form, query=query)
+
+
+if '__main__' == __name__:
+    app.run(debug=True)
