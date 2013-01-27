d3e9ec5b04e32d760f49cfa8be963bd2cf6c2eb6
Kevin Teh <kkwteh@gmail.com>
2013-01-26 21:01:46 -0800

    displaying vanilla twitter results

--- web_app/disambiguator.py
+++ web_app/disambiguator.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding=utf8
+import twitter
+import unicodedata
 
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
@@ -15,15 +17,31 @@ app.config['BOOTSTRAP_USE_CDN'] = True
 app.config['BOOTSTRAP_FONTAWESOME'] = True
 app.config['SECRET_KEY'] = 'devkey'
 
+
 class QueryForm(Form):
     q = TextField(' ',validators=[Required()])
+
 
 @app.route('/')
 def index():
     form = QueryForm()
     query = request.args.get('q')
-    return render_template('index.html', form=form, query=query)
+    tweets = get_tweets(query)
+    return render_template('index.html', form=form, tweets=tweets)
 
+
+def get_tweets(query):
+    search = twitter_search.search(q=query, lang="en")
+    return [get_ascii(t[u'text']) for t in search[u'results']]
+
+
+def init_twitter():
+    return twitter.Twitter(domain="search.twitter.com")
+
+def get_ascii(u_string):
+    return unicodedata.normalize('NFKD', u_string).encode('ascii','ignore')
 
 if '__main__' == __name__:
+    twitter_search = init_twitter()
     app.run(debug=True)
+
