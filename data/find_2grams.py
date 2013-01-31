a55b536e3f0ea161ad4a0e15cb9e44c731d76137
Kevin Teh <kkwteh@gmail.com>
2013-01-31 12:10:43 -0800

    removed data folder (contained ngram data) from repo

--- data/find_2grams.py
+++
@@ -1,9 +0,0 @@
-import re
-number = "45"
-input_file = 'googlebooks-eng-1M-2gram-20090715-' + number + '.csv'
-f = open(input_file,'r')
-for index, line in enumerate(f.xreadlines()):
-    if re.search('-', line) == None:
-            print(line)
-
-f.close()
