23422b10058012aa6e88199370508bb22140abb0
Kevin Teh <kkwteh@gmail.com>
2013-02-02 13:06:02 -0800

    removed unused form library from app

--- web_app/disambiguator.py
+++ web_app/disambiguator.py
@@ -7,8 +7,6 @@ import recommender
 import json
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
-from flask.ext.wtf import Form, TextField, HiddenField, ValidationError,\
-                          Required, RecaptchaField
 from flask import request
 
 app = Flask(__name__)
@@ -18,13 +16,9 @@ app.config['BOOTSTRAP_USE_CDN'] = True
 app.config['BOOTSTRAP_FONTAWESOME'] = True
 app.config['SECRET_KEY'] = 'devkey'
 
-class QueryForm(Form):
-    q = TextField(' ',validators=[Required()])
-
 
 @app.route('/')
 def index():
-    form = QueryForm()
     query = request.args.get('q')
 
     if request.args.get('recs') == "False":
@@ -46,7 +40,7 @@ def index():
             recommendations = recommender.find(query, cliques)
             recommendations = recommendations[:3]
 
-    return render_template('index.html', form=form, query=query, tweets=tweets,
+    return render_template('index.html', query=query, tweets=tweets,
                             count=count, keys=keys, len=len(keys),
                             recommendations=recommendations, graph=G)
 
