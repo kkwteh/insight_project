a487ec005f9bc9e4097caa4b8c3c6e35d7b5809f
Kevin Teh <kkwteh@gmail.com>
2013-02-04 12:35:28 -0800

    developed database counting scripts

--- counting_sandbox/interpret_words.py
+++ counting_sandbox/interpret_words.py
@@ -3,9 +3,16 @@
 import pickle
 import pdb
 import operator
+from nltk.corpus import wordnet as wn
 
+dolphins = ['google', 'apple', 'youtube',  'facebook',  'beckham',  'superbowl',  'justin',  'jaibrooks',  'jaifollowmeyoupeasant',   'beyonce', 'justinbieber',   '49ers',   'reina',  'liam',  'bieber',  'ipad',  'beliebers',  'chris',  'gerrard',  'joe',  'starbucks',  'lakers',  'edsheeran',  'itunes',  'rihanna',  'hagel',  'chelsea',  'selena',  'steve',  'jim',  'sturridge',  'nba',  'netflix',  'jackie',  'mcdonalds', 'kevin',  'steven',  'balotelli',  'xbox',  'francisco',  'skype',  'richard',  'espn', 'ravens', 'red', 'iphone', 'instagram', 'french', 'uk', 'american', 'london', 'english', 'internet', 'basketball', 'players', 'health', 'public', 'america', 'heaven']
 f = open("twitter_word_count.pkl", "r")
 count = pickle.load(f)
 sorted_count = sorted(count.iteritems(), key=operator.itemgetter(1))
 sorted_count.reverse()
-pdb.set_trace()
+sorted_count = [(a,b) for (a,b) in sorted_count if (b > 100 and
+                                                a not in dolphins)]
+f.close()
+output = open("top_twitter_words.pkl", "wb")
+pickle.dump(sorted_count, output, -1)
+output.close()
