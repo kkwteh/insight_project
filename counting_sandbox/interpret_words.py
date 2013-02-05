74d76b4462753c013aeb49d744cd1999bac8db80
Kevin Teh <kkwteh@gmail.com>
2013-02-04 17:58:27 -0800

    cutting words out of dolphins list. lesson -- be careful about adding words to this list

--- counting_sandbox/interpret_words.py
+++ counting_sandbox/interpret_words.py
@@ -5,7 +5,7 @@ import pdb
 import operator
 from nltk.corpus import wordnet as wn
 
-dolphins = ['google', 'apple', 'facebook',  'beckham',  'superbowl',  'justin',  'jaibrooks',  'jaifollowmeyoupeasant', 'justinbieber',   '49ers',   'reina',  'liam',  'bieber',  'ipad',  'beliebers',  'chris',  'gerrard',  'joe',  'starbucks',  'lakers',  'edsheeran',  'itunes',  'rihanna',  'hagel',  'chelsea',  'selena',  'steve',  'jim',  'sturridge',  'nba',  'netflix',  'jackie',  'mcdonalds', 'kevin',  'steven',  'balotelli',  'xbox',  'francisco',  'skype',  'richard',  'espn', 'ravens', 'iphone', 'instagram', 'french', 'uk', 'american', 'london', 'english', 'internet', 'basketball', 'players', 'health', 'public', 'america', 'heaven']
+dolphins = ['google', 'apple',  'beckham',  'superbowl',  'justin',  'jaibrooks',  'jaifollowmeyoupeasant', 'justinbieber',   '49ers',   'reina',  'liam',  'bieber',  'ipad',  'beliebers',  'chris',  'gerrard',  'joe',  'starbucks',  'lakers',  'edsheeran',  'itunes',  'rihanna',  'hagel',  'chelsea',  'selena',  'steve',  'jim',  'sturridge',  'nba',  'netflix',  'jackie',  'mcdonalds', 'kevin',  'steven',  'balotelli',  'xbox',  'francisco',  'skype',  'richard',  'espn', 'ravens', 'iphone', 'instagram', 'french', 'uk', 'american', 'london', 'english', 'internet', 'basketball', 'players', 'health', 'public', 'america', 'heaven']
 f = open("twitter_word_count.pkl", "r")
 count = pickle.load(f)
 sorted_count = sorted(count.iteritems(), key=operator.itemgetter(1))
