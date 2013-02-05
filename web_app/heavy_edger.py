ba4488f6cdd79b21b72f83cce046ba55ebfbee12
Kevin Teh <kkwteh@gmail.com>
2013-02-05 09:17:45 -0800

    fixed bug where candidate words were incorrectly extracted from query triple

--- web_app/heavy_edger.py
+++ web_app/heavy_edger.py
@@ -12,15 +12,15 @@ def compute_graph(query, top_results, tweets):
     i = 0
     query_list, page_nums = [],[]
     for w1, w2 in pairs:
-        query_list.append(query + " " + w1 + " " + w2)
+        query_list.append(w1 + " " + w2 + " " + query)
         page_nums.append(1)
     random.shuffle(query_list)
 
     per_page = 15
     pages_with_queries  = pages_getter.get_pages_of_tweets(searcher, query_list, page_nums, per_page)
     for query, page in pages_with_queries:
-        w1 = query.split()[1]
-        w2 = query.split()[2]
+        w1 = query.split()[0]
+        w2 = query.split()[1]
 
         for tweet in page[:]:
             text = tweet[u'text'].lower()
