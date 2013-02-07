982cf45ca9943ac2fb6e52c739bbb11a9d2cd9d3
Kevin Teh <kkwteh@gmail.com>
2013-02-06 21:23:52 -0800

    enabled toggling of recommendations

--- web_app/static/index.js
+++ web_app/static/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  $('h1').highlight('See');
+	$('.toggler').click(function() {
+		if ($('.hide_seek').hasClass('hide')){
+		  $('.toggler').text("Hide Recommendations");
+		  $('.hide_seek').removeClass('hide');
+		}else{
+  		  $('.toggler').text("Show Recommendations");
+  		  $('.hide_seek').addClass('hide');
+		}
+	});
 });
