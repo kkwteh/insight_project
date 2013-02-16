4e8639400c076aeba04f999bc82fad6060ef2bca
Kevin Teh <kkwteh@gmail.com>
2013-02-16 13:08:44 -0800

    refreshed twitter oauth credentials

--- cred.py
+++ cred.py
@@ -1,1 +1,1 @@
-twitter_OAuth = ('101769689-tPwXbgj96kaYpnCKHSijZJ5r6arePyLlMIQUj4Ts', 'ijCLoaw3bfRiOzbR572jKGQe3pYHndIps3CIp9KOWa4', 'sLGccwOdfySptswo1ZKErg', 'z5V9g6sOJ9BEYhvsSvnzt6pjS7gVWV2komWyIz5XZE')
+twitter_OAuth = ('101769689-tPwXbgj96kaYpnCKHSijZJ5r6arePyLlMIQUj4Ts', 'ijCLoaw3bfRiOzbR572jKGQe3pYHndIps3CIp9KOWa4', 'qdjzOr4PXIFLxF9U8NvqAQ', 'cFGSdXgENsXrukrsq1hngZlOPD4cCNqC68csYG94I')
